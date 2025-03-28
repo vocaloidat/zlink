bd4d9bb5239ed9863c58f3b585bbf804b2788114
vocaloidat <vocaloid.at@gmail.com>
2025-03-29 00:26:37 +0800

    修复bug

--- zurl/internal/handler/showhandler.go
+++ zurl/internal/handler/showhandler.go
@@ -1,10 +1,11 @@
 package handler
 
 import (
+	"errors"
+	"github.com/go-playground/validator/v10"
+	"github.com/zeromicro/go-zero/rest/httpx"
 	"net/http"
 	"zlink/zurl/bodyx"
-
-	"github.com/zeromicro/go-zero/rest/httpx"
 	"zlink/zurl/internal/logic"
 	"zlink/zurl/internal/svc"
 	"zlink/zurl/internal/types"
@@ -18,14 +19,22 @@ func ShowHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 			return
 		}
 
+		// 自定义参数规则校验
+		err := validator.New().StructCtx(r.Context(), &req)
+		if err != nil {
+			httpx.ErrorCtx(r.Context(), w, errors.New("长连接参数错误"))
+			return
+		}
+
 		l := logic.NewShowLogic(r.Context(), svcCtx)
 		resp, err := l.Show(&req)
-		//if err != nil {
-		//	httpx.ErrorCtx(r.Context(), w, err)
-		//} else {
-		//	httpx.OkJsonCtx(r.Context(), w, resp)
-		//}
-		// 自定义结构返回
-		bodyx.Writer(w, r, resp, err)
+		if err != nil {
+			// 自定义结构返回
+			bodyx.Writer(w, r, resp, err)
+		} else {
+			// 跳转
+			http.Redirect(w, r, resp.LongUrl, http.StatusFound)
+		}
+
 	}
 }
--- zurl/internal/logic/showlogic.go
+++ zurl/internal/logic/showlogic.go
@@ -2,6 +2,8 @@ package logic
 
 import (
 	"context"
+	"database/sql"
+	"errors"
 
 	"zlink/zurl/internal/svc"
 	"zlink/zurl/internal/types"
@@ -26,6 +28,18 @@ func NewShowLogic(ctx context.Context, svcCtx *svc.ServiceContext) *ShowLogic {
 // Show 展示短链接
 func (l *ShowLogic) Show(req *types.ShowRequest) (resp *types.ShowResponse, err error) {
 	//
+	// 数据库查询短链接对应长连接
+	u, err := l.svcCtx.ZUrlModel.FindOneBySurl(l.ctx, sql.NullString{
+		String: req.ShortUrl,
+		Valid:  true,
+	})
+	if err != nil {
+		logx.Error("数据库查询错误:err:", err)
+		return nil, errors.New("请输入正确的链接")
+	}
 
-	return
+	// 返回短链接对应的长链接
+	return &types.ShowResponse{
+		LongUrl: u.Lurl.String,
+	}, nil
 }
